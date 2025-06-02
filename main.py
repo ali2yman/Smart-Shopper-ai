570cab23b57b17e3c573818c3bfd4cb60b7b6c8a
ali2yman <ali.ayman5755@gmail.com>
2025-06-02 22:09:21 +0300

    Setup The Project

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from crewai import Agent,Task,Crew,Process,LLM
+from crewai import LLM
 import agentops
 import os
-import requests
 from dotenv import load_dotenv
 
 
@@ -12,5 +11,16 @@ AGENT_OPS_KEY = os.getenv("AGENTOPS_API_KEY")
 
 
 
+agentops.init(
+    api_key=AGENT_OPS_KEY,
+    skip_auto_end_session=True,
+    default_tags=["crewai"],
+)
 
+basic_llm = LLM(
+    api_key=LLM_API_KEY,
+    base_url="https://openrouter.ai/api/v1",
+    model = "openrouter.ai/deepseek/deepseek-r1-0528",
+    temperature=0
+)
 
