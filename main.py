9106b79b3b604d7ead796a0a5d0ef9a1a3278725
ali2yman <ali.ayman5755@gmail.com>
2025-06-05 08:52:11 +0300

    upate

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # main.py  ─  SmartShopper AI – Rankyx Procurement Assistant
 # ---------------------------------------------------------------------------
 
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# __import__('pysqlite3')
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 
 import os, traceback
